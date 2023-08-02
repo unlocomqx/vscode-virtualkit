4e8df6410fb924c864736904e55f2dd042da6b98
unloco <amxmodx.unloco@gmail.com>
2023-08-02 07:25:12 +0100

    add activationEvents

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "virtualkit" is now active!');
+	console.log('VirtualKit Activated!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('virtualkit.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VirtualKit!');
+		vscode.window.showInformationMessage('Hello VS Code');
 	});
 
 	context.subscriptions.push(disposable);
